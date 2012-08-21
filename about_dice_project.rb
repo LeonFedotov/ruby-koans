2e8a5354725b27ac1e06c0603c44cc29f356d8ba
Your Name <fedotov.leon@gmail.com>
2012-08-22 00:46:34 +0300

    end open classes

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -5,7 +5,12 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 # class DiceSet
 #   code ...
 # end
-
+class DiceSte 
+  attr_reader :values
+  def roll(rolls)
+    @values = rolls.times.map {rand(6)+1}
+  end
+end
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
