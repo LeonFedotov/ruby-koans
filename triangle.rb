06c015960af466437ea09eae6bacb93ea446e449
Your Name <fedotov.leon@gmail.com>
2012-08-21 23:29:01 +0300

    triangles rewrite :D

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  return :equilateral if a == b && b == c
-  return :isosceles if ((a == b && a != c) or (a == c && a != b) or (c == b && c != a))
-  return :scalene if (a != b && b != c && c != a)
+  a, b, c = [a, b, c].sort
+  raise TriangleError if a <= 0 || a + b <= c
+  [nil, :equilateral, :isosceles,:scalene][[a,b,c].uniq.size]
 end
 
 # Error class used in part 2.  No need to change this code.
